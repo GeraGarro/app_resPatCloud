84728b54eadb106214c58435553b40244c740673
GeraGarro <geragarro93@gmail.com>
2025-04-07 00:47:53 -0300

    07_04_05 segunda actualizacion 2

--- ResiduoPat_FrontEnd/src/environments/environment.prod.ts
+++ ResiduoPat_FrontEnd/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
 /*  apiUrl: 'http://localhost:8080/api' */
-    apiUrl: 'http://vps-4679263-x.dattaweb.com:8080/api' , 
+   
+    apiUrl: '/api' , 
 
   };
